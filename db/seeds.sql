d109cb0d0793e71785f6750f592b7230c6627660
Matt Milkovich <mattmilkovich@MacBook-Pro.local>
2019-01-07 01:44:04 -0600

    File-Set-Up

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUE ("hamburger", false), ("cheeseburger", false), ("veggieburger", false);
+
+SELECT * FROM burgers;
---
+++ models/burger.js
@@ -0,0 +1,27 @@
+var orm = require("../config/orm.js");
+
+// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+// Export at the end of the burger.js file.
+
+
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
